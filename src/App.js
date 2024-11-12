547900defd9a8187092ca1e7d5994bf7ea4a35b8
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-12 09:34:14 +0200

    chahge files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react'
+import MyButton from './components/UI/button/MyButton'
+import './styles/App.css'
+import PostList from './components/PostList'
+import MyInput from './components/UI/input/MyInput'
 
-function App() {
+export default function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript 1', body: 'Decription' },
+    { id: 2, title: 'JavaScript 2', body: 'Decription' },
+    { id: 3, title: 'JavaScript 3', body: 'Decription' },
+  ])
+  const [post, setPost] = useState({ title: '', body: '' })
+
+  // const bodyInputRef = useRef()
+
+  function addNewPost(e) {
+    e.preventDefault()
+    const newPost = {
+      title: post.title,
+      body: post.body,
+      id: Date.now(),
+    }
+    setPosts([...posts, newPost])
+    setPost((prev) => ({
+      ...prev,
+      title: '',
+      body: '',
+    }))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <form>
+        {/* Управляемый компонент */}
+        <MyInput
+          type="text"
+          placeholder="Название поста"
+          value={post.title}
+          onChange={(e) =>
+            setPost((prev) => ({
+              ...prev,
+              title: e.target.value,
+            }))
+          }
+        />
 
-export default App;
+        {/* Неуправляемый компонент
+        <MyInput ref={bodyInputRef} type="text" placeholder="Описание поста" /> */}
+
+        <MyInput
+          type="text"
+          placeholder="Описание поста"
+          value={post.body}
+          onChange={(e) =>
+            setPost((prev) => ({
+              ...prev,
+              body: e.target.value,
+            }))
+          }
+        />
+
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Список постов про JS" />
+    </div>
+  )
+}
