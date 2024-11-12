cad627396e8731921a52001a5ac17ecab54f54ab
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-12 13:56:46 +0200

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import './styles/App.css'
 import PostList from './components/PostList'
 import PostForm from './components/PostForm'
 import MySelect from './components/UI/select/MySelect'
+import MyInput from './components/UI/input/MyInput'
 
 export default function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'JavaScript 1', body: 'Decription' },
-    { id: 2, title: 'JavaScript 2', body: 'Decription' },
-    { id: 3, title: 'JavaScript 3', body: 'Decription' },
+    { id: 1, title: 'JavaScript ', body: 'JavaScript Decription' },
+    { id: 2, title: 'Node.js', body: 'Node Decription' },
+    { id: 3, title: 'Python', body: 'Python Decription' },
   ])
 
   const [selectedSort, setSelectedSort] = useState('')
+  const [search, setSearch] = useState('')
+
+  const sortedPost = useMemo(() => {
+    console.log('есть')
+    if (selectedSort) {
+      return [...posts].sort((a, b) =>
+        a[selectedSort].localeCompare(b[selectedSort])
+      )
+    } else {
+      return posts
+    }
+  }, [selectedSort, posts])
 
   function createPost(newPo) {
     setPosts([...posts, newPo])
@@ -23,31 +36,37 @@ export default function App() {
 
   function sortPosts(sort) {
     setSelectedSort(sort)
-    console.log(sort)
   }
 
   return (
     <div className="App">
       <PostForm create={createPost} />
       <hr style={{ margin: '15px 0' }} />
-      <MySelect
-        value={selectedSort}
-        onChange={sortPosts}
-        defaultValue="Сортировка:"
-        options={[
-          { value: 'title', name: 'По названию' },
-          { value: 'body', name: 'По описанию' },
-        ]}
-      />
-      {posts.length === 0 ? (
-        <h1 style={{ textAlign: 'center' }}>Посты не найдены</h1>
-      ) : (
-        <PostList
-          remove={removePost}
-          posts={posts}
-          title="Список постов про JS"
+      <div>
+        <MyInput
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placеholder="Поиск..."
         />
-      )}
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка:"
+          options={[
+            { value: 'title', name: 'По названию' },
+            { value: 'body', name: 'По описанию' },
+          ]}
+        />
+        {posts.length === 0 ? (
+          <h1 style={{ textAlign: 'center' }}>Посты не найдены</h1>
+        ) : (
+          <PostList
+            remove={removePost}
+            posts={sortedPost}
+            title="Список постов про JS"
+          />
+        )}
+      </div>
     </div>
   )
 }
