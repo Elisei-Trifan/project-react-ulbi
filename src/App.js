f6e0a55ed43922f541c949db27419c1f9e493e57
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-15 11:54:22 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,97 +1,20 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import Navbar from './components/UI/navbar/Navbar'
+import About from './pages/About'
+import Posts from './pages/Posts'
 import './styles/App.css'
-import PostList from './components/PostList'
-import PostForm from './components/PostForm'
-import PostFilter from './components/PostFilter'
-import MyModal from './components/UI/modal/MyModal'
-import MyButton from './components/UI/button/MyButton'
-import { usePost } from './hooks/usePost'
-import PostService from './API/PostServece'
-import Loader from './components/UI/loader/Loader'
-import { useFetching } from './hooks/useFetching'
-import { getPageCount, getPagesArray } from './utils/pages'
-import Pagination from './components/UI/pagination/Pagination'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 export default function App() {
-  const [posts, setPosts] = useState([])
-
-  const [filter, setFilter] = useState({ sort: '', query: '' })
-  const [modal, setModal] = useState(false)
-  const [totalPage, setTotalPage] = useState(0)
-  const [limit, setLimit] = useState(10)
-  const [page, setPage] = useState(1)
-
-  const [fetchPost, isPostLoading, postError] = useFetching(
-    async (limit, page) => {
-      const response = await PostService.getAll(limit, page)
-      setPosts(response.data)
-      const totalCount = response.headers['x-total-count']
-
-      setTotalPage(getPageCount(totalCount, limit))
-    }
-  )
-
-  const sortedAndSearchadPost = usePost(posts, filter.sort, filter.query)
-
-  useEffect(() => {
-    fetchPost(limit, page)
-  }, [])
-
-  function createPost(newPo) {
-    setPosts([...posts, newPo])
-    setModal(false)
-  }
-
-  function removePost(removePo) {
-    setPosts(posts.filter((item) => item.id !== removePo.id))
-  }
-
-  const changePageMemo = useMemo(() => {
-    function changePage(page) {
-      setPage(page)
-      fetchPost(limit, page)
-    }
-    return changePage
-  }, [page])
-
   return (
-    <div className="App">
-      <MyButton style={{ marginTop: '30px' }} onClick={() => setModal(true)}>
-        Создать пост
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-
-      <hr style={{ margin: '15px 0' }} />
-
-      <PostFilter filter={filter} setFilter={setFilter} />
-
-      {postError && <h1>Произошла ошибка {postError} </h1>}
-
-      {isPostLoading ? (
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            marginTop: '50px',
-          }}
-        >
-          <Loader />
-        </div>
-      ) : (
-        <PostList
-          remove={removePost}
-          posts={sortedAndSearchadPost}
-          title="Список постов про JS"
-        />
-      )}
-      <Pagination
-        page={page}
-        changePageMemo={changePageMemo}
-        totalPage={totalPage}
-      />
-    </div>
+    <BrowserRouter>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/posts" element={<Posts />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Posts.jsx
@@ -0,0 +1,97 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useMemo, useState } from 'react'
+import PostList from '../components/PostList'
+import PostForm from '../components/PostForm'
+import PostFilter from '../components/PostFilter'
+import MyModal from '../components/UI/modal/MyModal'
+import MyButton from '../components/UI/button/MyButton'
+import { usePost } from '../hooks/usePost'
+import PostService from '../API/PostServece'
+import Loader from '../components/UI/loader/Loader'
+import { useFetching } from '../hooks/useFetching'
+import { getPageCount } from '../utils/pages'
+import Pagination from '../components/UI/pagination/Pagination'
+
+export default function Posts() {
+  const [posts, setPosts] = useState([])
+
+  const [filter, setFilter] = useState({ sort: '', query: '' })
+  const [modal, setModal] = useState(false)
+  const [totalPage, setTotalPage] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(1)
+
+  const [fetchPost, isPostLoading, postError] = useFetching(
+    async (limit, page) => {
+      const response = await PostService.getAll(limit, page)
+      setPosts(response.data)
+      const totalCount = response.headers['x-total-count']
+
+      setTotalPage(getPageCount(totalCount, limit))
+    }
+  )
+
+  const sortedAndSearchadPost = usePost(posts, filter.sort, filter.query)
+
+  useEffect(() => {
+    fetchPost(limit, page)
+  }, [])
+
+  function createPost(newPo) {
+    setPosts([...posts, newPo])
+    setModal(false)
+  }
+
+  function removePost(removePo) {
+    setPosts(posts.filter((item) => item.id !== removePo.id))
+  }
+
+  const changePageMemo = useMemo(() => {
+    function changePage(page) {
+      setPage(page)
+      fetchPost(limit, page)
+    }
+    return changePage
+  }, [page])
+
+  return (
+    <div className="App">
+      <MyButton style={{ marginTop: '30px' }} onClick={() => setModal(true)}>
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+
+      <hr style={{ margin: '15px 0' }} />
+
+      <PostFilter filter={filter} setFilter={setFilter} />
+
+      {postError && <h1>Произошла ошибка {postError} </h1>}
+
+      {isPostLoading ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '50px',
+          }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchadPost}
+          title="Список постов про JS"
+        />
+      )}
+      <Pagination
+        page={page}
+        changePageMemo={changePageMemo}
+        totalPage={totalPage}
+      />
+    </div>
+  )
+}
